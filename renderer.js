c3e03e3a675cd6c9564b198eee10ab994765c6d8
Erik Olson <eolson@horizontalintegration.com>
2019-06-18 15:05:03 -0500

    Adds basic search functionality and Font-Awesome

--- renderer.js
+++ renderer.js
@@ -4,28 +4,34 @@ const {remote} = require('electron')
 require('popper.js');
 require('bootstrap');
 
-function buildPage() {
-  var ajaxRequest = new XMLHttpRequest();
+function buildPage(search) {
+  let ajaxRequest = new XMLHttpRequest();
   ajaxRequest.onreadystatechange = function() {
     if(ajaxRequest.readyState == 4) {
       if(ajaxRequest.status == 200) {
         let dataObject = JSON.parse(ajaxRequest.responseText);
         let count = 0;
         let listItem;
-
-        dataObject.forEach(buildNewList);
+        $('#songlist tr').remove();
+        if(search) {
+          let filteredRes = getObjects(dataObject,'artist',search);
+          filteredRes.forEach(buildNewList);
+        } else {
+          dataObject.forEach(buildNewList);
+        }
 
         function buildNewList(item, index) {
-          console.log(count);
           if (count === 0) {
             listItem = $('<tr><th>Artist Name</th><th>Song Title</th><th>Rating</th></tr><tr><td>' + item.artist + '</td><td><a href="#" class="opener" data-tab="' + item.filename + '">' + item.title + '</a></td><td>' + item.rating + '</td></tr>');
             count++;
           } else {
-            listItem = $('<tr><td>' + item.artist + '</td><td><a href="#" class="opener" data-tab="' + item.filename + '">' + item.title + '</a></td><td>' + item.rating + '</td></tr>');}
+            listItem = $('<tr><td>' + item.artist + '</td><td><a href="#" class="opener" data-tab="' + item.filename + '">' + item.title + '</a></td><td>' + item.rating + '</td></tr>');
+          }
           $('#songlist').append(listItem);
         }
 
         buildLinks();
+
       } else {
         console.log("Status error: " + ajaxRequest.status);
       }
@@ -50,4 +56,71 @@ function buildLinks() {
   });
 }
 
-window.onload = buildPage;
+function search() {
+  let search = $('#searchInput').val();
+  buildPage(search);
+}
+
+$(function() {
+  $('#searchBtn').on('click', function(e) {
+    search();
+  });
+  $('#searchInput').on('keyup', function(e) {
+    console.log(e);
+    if (e.keyCode == 13) {
+      search();
+    } 
+  });
+});
+
+// Returns an array of objects according to key, value, or key and value matching
+function getObjects(obj, key, val) {
+  let objects = [];
+  for (let i in obj) {
+    if (!obj.hasOwnProperty(i)) continue;
+    if (typeof obj[i] == 'object') {
+      objects = objects.concat(getObjects(obj[i], key, val));    
+    } else 
+    // If key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
+    if (i == key && obj[i] == val || i == key && val == '') { //
+      objects.push(obj);
+    } else if (obj[i] == val && key == ''){
+      // Only add if the object is not already in the array
+      if (objects.lastIndexOf(obj) == -1){
+        objects.push(obj);
+      }
+    }
+  }
+  return objects;
+}
+
+// Returns an array of values that match on a certain key
+function getValues(obj, key) {
+  let objects = [];
+  for (let i in obj) {
+    if (!obj.hasOwnProperty(i)) continue;
+    if (typeof obj[i] == 'object') {
+      objects = objects.concat(getValues(obj[i], key));
+    } else if (i == key) {
+      objects.push(obj[i]);
+    }
+  }
+  return objects;
+}
+
+// Returns an array of keys that match on a certain value
+function getKeys(obj, val) {
+  let objects = [];
+  for (let i in obj) {
+    if (!obj.hasOwnProperty(i)) continue;
+    if (typeof obj[i] == 'object') {
+      objects = objects.concat(getKeys(obj[i], val));
+    } else if (obj[i] == val) {
+      objects.push(i);
+    }
+  }
+  return objects;
+}
+
+
+window.onload = buildPage();
